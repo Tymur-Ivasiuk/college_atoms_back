5dbc38a47dec178a67461f4fe8250b4ebb392beb
KeyaruUzumaki <92820887+KeyaruUzumaki@users.noreply.github.com>
2023-06-30 23:34:21 +0300

    add requirements.txt n new api GAS link

--- zvit/viewsApi.py
+++ zvit/viewsApi.py
@@ -5,7 +5,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def subscribers(request):
-    url = "https://script.google.com/macros/s/AKfycbz28vJX12t_YhijaRkqaBz_KSGoUIXCnY4zGGVXRgCEGHBI5-qS5Pins5iNVTit9p8O/exec"
+    url = "https://script.google.com/macros/s/AKfycbwvaVas2ZEQejU3Cqd7E23uwim7I7puLvf6MFJXJWcgA9V05Ot7APQssMLy2ueWyq8R/exec"
 
     data = dict(request.POST)
     data['password'] = '1234'
